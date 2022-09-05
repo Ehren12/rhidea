081bfb7479ff0cba02d9e9598196ab77a3bb1764
Ehren Nwokocha <nwokochaehren@gmail.com>
2022-09-05 08:31:51 +0100

    add declaration file

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-declare module 'wtf-8';
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
---
+++ wtf-8.d.ts
@@ -0,0 +1,1 @@
+declare module 'wtf-8';
